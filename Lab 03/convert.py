631b0d1ad07064bccf25f057c1d5573feb6cba53
jancion <julian.ancion91@gmail.com>
2017-09-18 14:06:37 -0400

    Do eeeet

--- Lab 03/convert.py
+++ Lab 03/convert.py
@@ -1,27 +1,8 @@
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-
+'''
+Julian Ancion + Prof. Ordonez
+9/18/17
+Convert
+'''
 
 def groupedByThrees(number):
     """
