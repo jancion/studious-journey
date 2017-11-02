32394862ceedb72916b7a6e5ecff8357d7953efc
jknecht <jknecht@southern.edu>
2017-11-02 16:53:27 -0400

    BOOTY

--- Jacob-s -Stuff/Warmup_10.py
+++ Jacob-s -Stuff/Warmup_10.py
@@ -23,15 +23,21 @@ class Fraction:
         >>> Fraction(5, 7)
         Fraction(5, 7)
         '''
-        GCD = self.compute_GCD()
+        GCD = self.__compute_GCD()
         self.numer = self.numer // GCD
         self.denom = self.denom // GCD
 
-    def compute_GCD(self):
+    def __compute_GCD(self):
         '''
-        Give two numbers, returns GCD
+        Code came from https://www.programiz.com/python-programming/examples/hcf
+        It is the Euclidean algorithm
+        >>> Fraction(-3,-6)
+        Fraction(1, 2)
+        >>> Fraction("-7/49")
+        Fraction(-1, 7)
+        >>> Fraction(5, 7)
+        Fraction(5, 7)
 
-        :return:
         '''
         x = self.numer
         y = self.denom
@@ -45,66 +51,138 @@ class Fraction:
         Fraction(1, 2)
         >>> Fraction("-7/49")
         Fraction(-1, 7)
-        >>> Fraction(5, 7)
-        Fraction(5, 7)
+        >>> Fraction(5, -7)
+        Fraction(-5, 7)
+        >>> Fraction(-2, -3)
+        Fraction(2, 3)
         '''
         if self.denom < 0:
             self.denom, self.numer = -self.denom, -self.numer
 
     def __repr__(self):
-        """Returns a string that, when evaluated, creates an identical object."""
+        '''
+        >>> Fraction(-3,-6)
+        Fraction(1, 2)
+        >>> Fraction("-7/49")
+        Fraction(-1, 7)
+        >>> Fraction(5, -7)
+        Fraction(-5, 7)
+        >>> Fraction(-2, -3)
+        Fraction(2, 3)
+        '''
         return "Fraction(%d, %d)" % (self.numer, self.denom)
 
     def __str__(self):
+        '''
+        >>> print(Fraction(1,2))
+        1/2
+        >>> print(Fraction(-5, 7))
+        -5/7
+        '''
         return "%d/%d" % (self.numer, self.denom)
 
     def __add__(self, other):
+        '''
+
+        >>> Fraction(1,2) + Fraction(-5, 7)
+        Fraction(-3, 14)
+        >>> Fraction(-8,-12) + Fraction(6, -3)
+        Fraction(-4, 3)
+        '''
         return Fraction(self.numer * other.denom + self.denom * other.numer, self.denom * other.denom)
 
     def __sub__(self, other):
+        '''
+        >>> Fraction(1,2) - Fraction(-5, 7)
+        Fraction(17, 14)
+        >>> Fraction(-8,-12) - Fraction(6, -3)
+        Fraction(8, 3)
+        '''
         return Fraction(self.numer * other.denom - self.denom * other.numer, self.denom * other.denom)
 
     def __mul__(self, other):
+        '''
+        >>> Fraction(1,2) * Fraction(-5, 7)
+        Fraction(-5, 14)
+        >>> Fraction(-8,-12) * Fraction(6, -3)
+        Fraction(-4, 3)
+        '''
         return Fraction(self.numer * other.numer, self.denom * other.denom)
 
     def __truediv__(self, other):
+        '''
+        >>> Fraction(1,2) / Fraction(-5, 7)
+        Fraction(-7, 10)
+        >>> Fraction(-8,-12) / Fraction(6, -3)
+        Fraction(-1, 3)
+        '''
         return Fraction(self.numer * other.denom, self.denom * other.numer)
 
     def __eq__(self, other):
-        return self.numer / self.denom == other.numer / other.denom
+        '''
+        >>> Fraction(1,2) == Fraction(1, 2)
+        True
+        >>> Fraction(-8,-12) == Fraction(6, -3)
+        False
+        '''
+        return str(self) == str(other)
 
     def __ne__(self, other):
-        return self.numer / self.denom != other.numer / other.denom
+        '''
+        >>> Fraction(1,2) != Fraction(1, 2)
+        False
+        >>> Fraction(-8,-12) != Fraction(6, -3)
+        True
+        '''
+        return str(self) != str(other)
 
     def __gt__(self, other):
-        return self.numer / self.denom > other.numer / other.denom
+        '''
+        >>> Fraction(1,2) > Fraction(1, 2)
+        False
+        >>> Fraction(-8,-12) > Fraction(6, -3)
+        True
+        '''
+        newSelf, newOther = self.common_denom(other)
+        return newSelf > newOther
 
     def __lt__(self, other):
-        return self.numer / self.denom < other.numer / other.denom
+        '''
+        >>> Fraction(1,2) < Fraction(1, 2)
+        False
+        >>> Fraction(-8,-12) < Fraction(6, -3)
+        False
+        '''
+        newSelf, newOther = self.common_denom(other)
+        return newSelf < newOther
 
     def __ge__(self, other):
-        return self.numer / self.denom >= other.numer / other.denom
+        '''
+        >>> Fraction(1,2) >= Fraction(1, 2)
+        True
+        >>> Fraction(-8,-12) >= Fraction(6, -3)
+        True
+        '''
+        newSelf, newOther = self.common_denom(other)
+        return newSelf >= newOther
 
     def __le__(self, other):
-        return self.numer / self.denom <= other.numer / other.denom
+        '''
+        >>> Fraction(1,2) <= Fraction(1, 2)
+        True
+        >>> Fraction(-8,-12) <= Fraction(6, -3)
+        False
+        '''
+        newSelf, newOther = self.common_denom(other)
+        return newSelf <= newOther
 
+    def common_denom(self, other):
+        newSelfNumer = self.numer * other.denom
+        newOtherNumer = other.numer * self.denom
 
+        return newSelfNumer, newOtherNumer
 
 
 if __name__ == "__main__":
     import doctest
-
     doctest.testmod()
-    fracZero = Fraction()
-    fracOne = Fraction(1)
-    fracHalf = Fraction(1, 2)
-    fracTwoThirds = Fraction("2/3")
-    # frac45hundredths = Fraction(0.45)
-    print("%s + %s = %s" % (fracHalf, fracTwoThirds, fracHalf + fracTwoThirds))
-    # print("%s - %s = %s" % (fracHalf, fracTwoThirds, fracHalf - fracTwoThirds))
-    # print("%s * %s = %s" % (fracHalf, fracTwoThirds, fracHalf * fracTwoThirds))
-    # print("%s / %s = %s" % (fracHalf, fracTwoThirds, fracHalf / fracTwoThirds))
-    if fracTwoThirds * Fraction(3) == Fraction(2):
-        print("Exact results achieved")
-    else:
-        print("You might as well use floats")
