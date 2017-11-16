3314ea0b1adacf890367e686b9053354f8607611
jake <jknecht@southern.edu>
2017-11-15 19:51:16 -0500

    BOOTYYYYY

--- Jacob-s -Stuff/duration_lab.py
+++ Jacob-s -Stuff/duration_lab.py
@@ -15,12 +15,12 @@ class Duration:
         if len(args) == 0:
             print('Please enter a time argument.')
         elif len(args) == 3 and type(args[0]) == int:
-            self.seconds = args[2]
-            self.minutes = args[1]
-            self.hours = args[0]
+            self.seconds = int(args[2])
+            self.minutes = int(args[1])
+            self.hours = int(args[0])
         elif len(args) == 1:
             if type(args[0]) == int:
-                self.seconds = args[0]
+                self.seconds = int(args[0])
                 if self.seconds > 60:
                     self.minutes = self.seconds // 60
                     self.seconds = self.seconds % 60
@@ -38,9 +38,10 @@ class Duration:
                         self.minutes = int(time_list[1])
                         self.seconds = int(time_list[2])
                     else:
-                        self.seconds = args[0]
-                else:
+                        self.seconds = int(args[0])
+                elif 'h' in args[0]:
                     hours_list = args[0].split('h')
+                    print(hours_list)
                     minutes_list = hours_list[1].split('m')
                     seconds_list = minutes_list[1].split('s')
                     self.hours = hours_list[0]
@@ -58,13 +59,23 @@ class Duration:
             return "Duration(%s:%s:%s)" % (str(self.hours), str(self.minutes), str(self.seconds))
 
     def __str__(self):
-        if self.hours == 0:
-            return "Duration(%s:%s)" % (str(self.minutes), str(self.seconds))
+        if self.seconds == 0:
+            return "Duration(%s:%s)" % (str(self.hours), str(self.minutes))
         else:
             return "Duration(%s:%s:%s)" % (str(self.hours), str(self.minutes), str(self.seconds))
 
+    def __mul__(self, other):
+        '''
+        >>> str(Duration(45) * (2 * 60))
+        'Duration(1:30)'
 
+        '''
+        return Duration(self.convert_to_seconds() * int(other))
+
+    def convert_to_seconds(self):
+        return int(self.hours * 3600 + self.minutes * 60 + self.seconds)
 if __name__ == "__main__":
-
-    print(Duration('8h23m47s'))
+    import doctest
+    doctest.testmod()
+    print(Duration('8h23m47s') * 60)
 
