3b1472dfe7ceb87ae61e057bb561112d60020904
jake <jknecht@southern.edu>
2017-11-22 20:27:35 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	1Julian's Stuff/Lab 11/guessinggame.py

--- 1Julian's Stuff/Lab 12/calculatorium.py
+++ 1Julian's Stuff/Lab 12/calculatorium.py
@@ -20,37 +20,98 @@ class CalculatorGuts:
         self.pending_operator = ''
         self.display = '0.'
         self.start_new_number = True
-        self.num_list = '0123456789'
-        self.operator_list = '$+-*/='
+        self.num_list = '0123456789.'
+        self.operator_list = '+-*/='
+        self.inverse_list = '$'
+        self.decimal_flag = False
+
+    def __repr__(self):
+        return "%s" % (self.display)
 
     def button_pressed(self, button):
         if button in self.num_list:
-            if self.start_new_number is True:
-                # Fresh calculator just turned on, replaces '0.' with pressed number
-                self.display = button
-                self.start_new_number = False
-            else:
-                # appends pressed button to number
-                self.display += button
+            self.number_pressed(button)
         elif button in self.operator_list:
-            if self.start_new_number is True:
-                # if '0.' and fresh number, have it do nothing if = is pressed
-                if button == '=':
-                    pass
-                else:
-                    # numbers have already been put in.
-                    self.pending_operator = button
+            self.operator_pressed(button)
+        elif button == self.inverse_list:
+            self.inverse()
+
+
+    def number_pressed(self, button):
+        if button == '.':
+            self.decimal_flag = True
+        if self.start_new_number is True:
+            # Fresh calculator just turned on, replaces '0.' with pressed number
+            self.display = button
+            self.start_new_number = False
+        else:
+        # appends pressed button to number
+            self.display += button
+        self.get_display()
+
+
+    def operator_pressed(self, button):
+        if self.start_new_number is True:
+            # if '0.' and fresh number, have it do nothing if = is pressed
+            if button == '=':
+                self.get_display
             else:
-                # after operand is assigned
+                # numbers have already been put in.
+                self.pending_operator = button
+        else:
+            # after operand is assigned
+            if button == "="
+                self.evaluate(button)
+            else:
                 self.left_operand = self.display
                 self.pending_operator = button
                 self.start_new_number = True
+                self.decimal_flag = False
 
-        pass
 
+    def evaluate(self, button):
+        if button == "+":
+            self.add()
+        if button == "-":
+            self.sub()
+        if button == "*":
+            self.multiply()
+        if button == "/":
+            self.division()
+
+    def add(self):
+        self.display = float(self.left_operand) + float(self.display)
+
+    def sub(self):
+        self.display = float(self.left_operand) - float(self.display)
+
+    def multiply(self):
+        self.display = float(self.left_operand) * float(self.display)
+
+    def division(self):
+        self.display = float(self.left_operand) / float(self.display)
+
+    def inverse(self):
+        self.display = str(-(float(self.display)))
+        if self.display[-1] == 0:
+            self.display = self.display[:-1]
     def buttons_pressed(self, buttons):
         for button in buttons:
             self.button_pressed(button)
 
+    def decimal_check(self):
+        if self.decimal_flag == False:
+            self.display += '.'
+            self.get_display()
+            self.display = self.display[:-1]
+        else:
+            self.get_display()
+
     def get_display(self):
-        pass
+        return self.display
+
+calc = CalculatorGuts()
+calc.button_pressed('6')
+calc.button_pressed('8')
+calc.button_pressed('.')
+calc.button_pressed('9')
--- Jacob-s -Stuff/Lab_11.py
+++ Jacob-s -Stuff/Lab_11.py
@@ -1,12 +1,10 @@
-from abc import ABC, abstractmethod
+from abc import ABC
 
 
 class Knowledge(ABC):
-    @abstractmethod
     def play(self):
         pass
 
-    @abstractmethod
     def learn(self):
         pass
 
@@ -16,8 +14,7 @@ class Nothing(Knowledge):
         return self.learn()
 
     def learn(self):
-        new_thing = input("What were you thinking of?")
-        print("Yay, I learned something new!")
+        new_thing = input("What were you thinking of? ")
         return Answer(new_thing)
 
 class Answer(Knowledge):
@@ -25,18 +22,21 @@ class Answer(Knowledge):
         self.answer = answer
 
     def play(self):
-        y_or_n = input("Were you thinking of %s (enter only Y or N)?" % self.answer)
+        y_or_n = input("Were you thinking of %s (enter only Y or N)? " % self.answer)
         if y_or_n == "Y":
             print("Aren't I smart?")
             return self
         else:
-            return self.learn
+            return self.learn()
 
     def learn(self):
-        new_answer = input("What were you thinking of?")
+        new_answer = input("What were you thinking of? ")
         new_question = input("What's a yes/no question I can use to distinguish between %s and %s? " % (self.answer, new_answer))
-        #ask for correct y/n, then use some if magic, self is yes branch, Answer is no branch
-        return Question(new_question, self, Answer(new_answer))
+        connect_Q2A = input("When I ask %s, does it belong to %s? (Answer with Y or N) " % (new_question, new_answer))
+        if connect_Q2A == 'Y':
+            return Question(new_question, Answer(new_answer), self)
+        else:
+            return Question(new_question, self, Answer(new_answer))
 
 class Question(Knowledge):
     def __init__(self, question, yes, no):
@@ -45,13 +45,11 @@ class Question(Knowledge):
         self.no = no
 
     def play(self):
-        new_answer = input(self.question + "Answer with a Y or N.")
-        if new_answer == "Y":
-            new_answer2 = input()
-
-
-    def learn(self):
-        pass
+        new_answer1 = input(self.question + " Answer with a Y or N. ")
+        if new_answer1 == "Y":
+            return self.yes.play()
+        else:
+            return self.no.play()
 
 if __name__ == "__main__":
     kb = Nothing()
